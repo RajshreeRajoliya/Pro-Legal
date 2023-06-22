4f5bc8c6b879c574767c4d07e50e9cdf3d4a6d50
RajshreeRajoliya <rajshreerajoliya14@gmail.com>
2023-06-22 21:48:27 +0530

    lastcommit

--- landing-page/src/App.jsx
+++ landing-page/src/App.jsx
@@ -7,9 +7,15 @@ import RightContainer from './component/RightContainer';
 
 const App= () => {
   const [features, setFeatures] = useState([]);
+
   const[page , setPage] = useState(1);
   const nextPage=()=>{
-    setPage((prev)=>prev+1)
+    if(page === 5){
+      setPage(1);
+    } else {
+      setPage((prev)=>prev+1)
+    }
+    
   }
   useEffect(() => {
     fetch("http://localhost:3001/data") // Adjust the URL if you're using a different route in json-server
@@ -20,16 +26,28 @@ const App= () => {
   }, []);
   return (
     <div className="App">
-     {/* <Navbar pic = {features[0].image} name = {features[0].name}/> */}
-      
+    
+   
      {
       features.slice(page*1 - 1 , page *1).map((ele)=>{
-        return <Navbar pic={ele.image} name={ele.name}/>
+        return <>
+        <Navbar pic={ele.image} name={ele.name} />
+     
+       <Sidebar/>
+       
+     <RightContainer operating_company={ele.operating_company} company_type={ele.company_type}
+      law_firm_or_business_name={ele.law_firm_or_business_name} address1={ele.address1} city={ele.city}
+      state={ele.state} country={ele.country} zipcode={ele.zipcode} phone_number={ele.phone_number}
+      firm_speciality={ele.firm_speciality} case_management={ele.case_management} account_manager={ele.account_manager}
+      new_user_invited={ele.new_user_invited} new_user_added={ele.new_user_added} payment_method_change={ele.payment_method_change}
+
+     />
+     <button onClick={nextPage} id='btn'>Next</button>
+        </>
       })
      }
-     <Sidebar/>
-     <RightContainer/>
-   <button onClick={nextPage}>NEXT</button>
+     
+    
      
     </div>
   );
--- landing-page/src/component/RightContainer.jsx
+++ landing-page/src/component/RightContainer.jsx
@@ -1,113 +1,116 @@
 import React from "react";
 import "./styles/RightContainer.css";
-function RightContainer() {
+function RightContainer(props) {
   return (
     <div className="rightContainer">
 
-    <div id="innerdiv">
-      <h1 id="heading">Company Information</h1>
+      <div id="innerdiv">
+        <h1 id="heading">Company Information</h1>
 
-      <div id="firstBox">
-        <div id="firstBoxfirstDiv">
-          <div style={{background:"#FFFFFF"}}>
-            <p style={{color:"#ADADAD" , background:"#FFFFFF"}}>Operating company</p>
-            
+        <div id="firstBox">
+          <div id="firstBoxfirstDiv">
+            <div >
+              <p style={{ color: "#ADADAD" }}>Operating company</p>
 
 
-            <div style={{background:"#FFFFFF",fontWeight:"bold" ,fontSize:"17px"}}>Pro Legal Serve</div>
+
+              <div style={{ fontWeight: "bold", fontSize: "17px" }}>{props.operating_company}</div>
+            </div>
+            <div >
+              <p style={{ color: "#ADADAD" }}>Company type</p>
+              <div style={{ fontWeight: "bold", fontSize: "17px" }}>{props.company_type}</div>
+            </div>
+            <div >
+              <p style={{ color: "#ADADAD" }}>Law firm or buisness name</p>
+              <div style={{ fontWeight: "bold", fontSize: "17px" }}>{props.law_firm_or_business_name}</div>
+            </div>
           </div>
-          <div style={{background:"#FFFFFF"}}>
-          <p style={{color:"#ADADAD" , background:"#FFFFFF"}}>Company type</p>
-          <div style={{background:"#FFFFFF",fontWeight:"bold" ,fontSize:"17px"}}>B2B</div>
+          <div id="firstBoxSecondDiv">
+            <div >
+              <p style={{ color: "#ADADAD" }}>Address 1</p>
+              <div style={{ fontWeight: "bold", fontSize: "17px" }}>{props.address1}</div>
+            </div>
           </div>
-          <div style={{background:"#FFFFFF"}}>
-          <p style={{color:"#ADADAD" , background:"#FFFFFF"}}>Law firm or buisness name</p>
-          <div style={{background:"#FFFFFF",fontWeight:"bold" ,fontSize:"17px"}}>Flash</div>
+          <div id="firstBoxThirdDiv">
+            <div >
+              <p style={{ color: "#ADADAD" }}>State</p>
+              <div >{props.state}</div>
+            </div>
+            <div >
+              <p style={{ color: "#ADADAD" }}>City</p>
+              <div >{props.city}</div>
+            </div>
+            <div >
+              <p style={{ color: "#ADADAD" }}>Country</p>
+              <div >{props.country}</div>
+            </div>
+          </div>
+          <div id="firstBoxFourthDiv">
+            <div >
+              <p style={{ color: "#ADADAD" }}>Zip code</p>
+              <div >{props.zipcode}</div>
+            </div>
+            <div >
+              <p style={{ color: "#ADADAD" }}>Phone number</p>
+              <div >{props.phone_number}</div>
+            </div>
+          </div>
+          <div class="SecondBoxThirdDiv">
+            <span style={{ color: "#ADADAD" }} >
+              -------------------------------------------------------------------------------------------------------------------
+            </span>
           </div>
         </div>
-        <div id="firstBoxSecondDiv">
-          <div style={{background:"#FFFFFF"}}>
-            <p style={{color:"#ADADAD" , background:"#FFFFFF"}}>Address 1</p>
-            <div style={{background:"#FFFFFF",fontWeight:"bold" ,fontSize:"17px"}}>Company Address</div>
+        <div className="thirdBlock"><h1>Law firm information</h1></div>
+        <div id="SecondBox">
+          <div id="SecondBoxfirstDiv">
+            <div >
+              <p style={{ color: "#ADADAD" }}>Firm Speciality</p>
+              <div >{props.firm_speciality}</div>
+            </div>
+            <div >
+              <p style={{ color: "#ADADAD" }}>Case management </p>
+              <div >{props.case_management}</div>
+            </div>
+          </div>
+          <div id="SecondBoxsecondDiv">
+            <div >
+              <p style={{ color: "#ADADAD" }}>Account manager</p>
+              <div >{props.account_manager}</div>
+            </div>
+          </div>
+          <div class="SecondBoxThirdDiv">
+            <span style={{ color: "#ADADAD" }}>
+            -------------------------------------------------------------------------------------------------------------------
+            </span>
+          </div>
+          <div id="lastBlock">
+            <h1 > Company Notification Setting</h1>
+          </div>
+          <div id="SecondBoxFifthDiv">
+            <div >
+              <p style={{ color: "#ADADAD" }}>New user invited</p>
+              <div >{props.new_user_invited}</div>
+            </div>
+            <div >
+              <p style={{ color: "#ADADAD" }}>New user invited</p>
+              <div >{props.new_user_added}</div>
+            </div>
+            <div >
+              <p style={{ color: "#ADADAD" }}>Law firm or Bussiness Name</p>
+              <div >{props.payment_method_change}</div>
+            </div>
+          </div>
+          <div class="SecondBoxThirdDiv">
+            <span style={{ color: "#ADADAD" }}>
+            -------------------------------------------------------------------------------------------------------------------
+            </span>
           </div>
         </div>
-        <div id="firstBoxThirdDiv">
-        <div style={{background:"#FFFFFF"}}>
-          <p style={{color:"#ADADAD" , background:"#FFFFFF"}}>State</p>
-            <div>ProLegal Serve</div>
-          </div>
-          <div style={{background:"#FFFFFF"}}>
-          <p style={{color:"#ADADAD" , background:"#FFFFFF"}}>City</p>
-            <div>B2B</div>
-          </div>
-          <div style={{background:"#FFFFFF"}}>
-          <p style={{color:"#ADADAD" , background:"#FFFFFF"}}>Country</p>
-            <div>Flash</div>
-          </div>
-        </div>
-        <div id="firstBoxFourthDiv">
-          <div>
-            <p>Zip code</p>
-            <div>ProLegal Serve</div>
-          </div>
-          <div>
-            <p>Phone number</p>
-            <div>B2B</div>
-          </div>
-        </div>
-        <div class="SecondBoxThirdDiv">
-          <span>
-          -------------------------------------------------------------------------------------------------------------------
-          </span>
-        </div>
+        
       </div>
-      <h1>Law firm information</h1>
-      <div id="SecondBox">
-        <div id="SecondBoxfirstDiv">
-          <div>
-            <p>Firm Speciality</p>
-            <div>Family Law</div>
-          </div>
-          <div>
-            <p>Case management </p>
-            <div>Server Manager</div>
-          </div>
-        </div>
-        <div id="SecondBoxsecondDiv">
-          <div>
-            <p>Account manager</p>
-            <div>Account Manager</div>
-          </div>
-        </div>
-        <div class="SecondBoxThirdDiv">
-          <span>
-            ------------------------------------------------------------------------------------------------------------------------
-          </span>
-        </div>
-        <div id="SecondBoxFourthDiv">
-          <h1> Company Notification Setting</h1>
-        </div>
-        <div id="SecondBoxFifthDiv">
-          <div>
-            <p>New user invited</p>
-            <div>Email</div>
-          </div>
-          <div>
-            <p>New user invited</p>
-            <div>Email</div>
-          </div>
-          <div>
-            <p>Law firm or Bussiness Name</p>
-            <div>Flash</div>
-          </div>
-        </div>
-        <div class="SecondBoxThirdDiv">
-          <span>
-            ------------------------------------------------------------------------------------------------------------------------
-          </span>
-        </div>
-      </div>
-      </div>
+
+
     </div>
   );
 }
